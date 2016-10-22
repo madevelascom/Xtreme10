7e7b25c525e1d97d2ae776f94e2e9992d1c6f16f
mvelasco93 <m_velasco93@hotmail.com>
2016-10-21 23:09:22 -0500

    inti

---
+++ Python/Made-Inti.py
@@ -0,0 +1,49 @@
+__author__ = 'madevelasco'
+
+import fileinput
+import itertools
+
+
+def gGDCcd(x,y):
+    while y != 0:
+        (x, y) = (y, x % y)
+    return x 
+
+def sum(alist):
+    x= 0
+    for i in alist:
+        x = x + int(i)
+    return x
+
+def main():
+    first = True
+    quan = 0
+    aset = []
+
+
+    for line in fileinput.input():
+        if (first):
+            quan = int(line)
+            first = False
+        else:
+            max = 0
+            line = str(line)
+            res = line.strip
+            res = line.split(' ')
+            res = [int(i) for i in res]
+            for L in range(1, len(res)+1):
+                for subset in itertools.combinations(res, L):
+                    a = reduce(gGDCcd, subset)
+                    if (a == 1):
+                        b = sum(subset)
+                        if(b > max):
+                            max = b
+
+
+            if (max > res[len(res)-1]):      
+                print (max)
+            else:
+                print(res[len(res)-1])
+
+if __name__ == '__main__':
+    main()
